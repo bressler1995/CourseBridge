9b54d54da0928ae0d0269986512ce7fa9bd54b0b
Brian Ressler <carlofabyss@hotmail.com>
2024-05-07 20:57:45 -0500

    Start scaffolding a UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import { Children } from 'react';
 import './App.css';
 import Module1 from './Module1.mdx';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
-    <div className="tops_os101">
-      <Module1 />
+    <div className='os101App'>
+      <Sidebar/>
+      <div className="os101Content">
+        <Module1 />
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+
+
+function Sidebar({children}) {
+  return (
+    <div className='os101Sidebar'></div>
+  )
+}
+
+export default Sidebar
