afeafbb7b90b1f9114558179ac94768c2ee378e5
Brian Ressler <carlofabyss@hotmail.com>
2024-05-22 13:16:52 -0500

    try setting parent iframe height from inside of document

--- src/components/UI/Content/Content.jsx
+++ src/components/UI/Content/Content.jsx
@@ -1,6 +1,10 @@
 import './Content.css';
 
 function Content({children, isMinimal = false}) {
+  if(isMinimal == true) {
+    window.parent.document.getElementById('myframe').height = '1000px';
+  }
+
   return (
     <div className={isMinimal == true ? 'os101Content minimalContent' : 'os101Content'}>
     {children}
