f230d5e21dec13f7e88133252783574337b7b68a
Brian Ressler <carlofabyss@hotmail.com>
2024-05-22 13:19:26 -0500

    remove parent setter code since that is setting of a CORS error on canvas

--- src/components/UI/Content/Content.jsx
+++ src/components/UI/Content/Content.jsx
@@ -1,9 +1,9 @@
 import './Content.css';
 
 function Content({children, isMinimal = false}) {
-  if(isMinimal == true) {
-    window.parent.document.getElementById('myframe').height = '1000px';
-  }
+  // if(isMinimal == true) {
+  //   window.parent.document.getElementById('myframe').height = '1000px';
+  // }
 
   return (
     <div className={isMinimal == true ? 'os101Content minimalContent' : 'os101Content'}>
