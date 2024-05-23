4bbfad121cabe13a6ff0017e2c371aba1d93f06e
Brian Ressler <carlofabyss@hotmail.com>
2024-05-23 01:49:36 -0500

    Move toggle button to topbar

--- src/components/UI/Sidebar/Sidebar.jsx
+++ src/components/UI/Sidebar/Sidebar.jsx
@@ -5,7 +5,6 @@ function Sidebar({children}) {
   return (
     <div className='os101Sidebar'>
       {children}
-      <button className='os101SidebarHide btn btn-primary'><BiChevronLeft/></button>
     </div>
   )
 }
--- src/components/UI/TopBar/TopBar.jsx
+++ src/components/UI/TopBar/TopBar.jsx
@@ -1,9 +1,10 @@
-
+import { BiMenuAltLeft } from "react-icons/bi";
+import './TopBar.css';
 
 function TopBar() {
   return (
     <div className="os101TopBar">
-      TopBar
+      <div className='os101Toggle_container'><button className='btn btn-primary os101Toggle'><BiMenuAltLeft /></button></div>
     </div>
   )
 }
