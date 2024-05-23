820bf6ea0163de849a79c5deae886b102e39081e
Brian Ressler <carlofabyss@hotmail.com>
2024-05-23 01:56:30 -0500

    Change arrow icon for top bar button

--- src/components/UI/TopBar/TopBar.jsx
+++ src/components/UI/TopBar/TopBar.jsx
@@ -1,10 +1,10 @@
-import { BiMenuAltLeft } from "react-icons/bi";
+import { BiArrowBack } from "react-icons/bi";
 import './TopBar.css';
 
 function TopBar() {
   return (
     <div className="os101TopBar">
-      <div className='os101Toggle_container'><button className='btn btn-primary os101Toggle'><BiMenuAltLeft /></button></div>
+      <div className='os101Toggle_container'><button className='btn btn-primary os101Toggle'><BiArrowBack /></button></div>
     </div>
   )
 }
