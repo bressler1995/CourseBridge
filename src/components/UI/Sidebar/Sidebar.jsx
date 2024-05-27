cc0f316359617c234aea50de573c9de30b1b17a2
Brian Ressler <carlofabyss@hotmail.com>
2024-05-26 22:45:17 -0500

    Add course title and version title

--- src/components/UI/Sidebar/Sidebar.jsx
+++ src/components/UI/Sidebar/Sidebar.jsx
@@ -3,7 +3,13 @@ import './Sidebar.css';
 function Sidebar({children, show}) {
   return (
     <div className={show == true ? 'os101Sidebar' : 'os101Sidebar hide'}>
-      <div className='os101Logo p-2 px-4'><img src='./images/logo.png'/></div>
+      <div className='os101Logo p-2 px-4'>
+        <img src='./images/logo.png'/>
+        <div className='os101Logo_courseInfo'>
+          <span>Open Science 101</span>
+          <span>Development Version</span>
+        </div>
+      </div>
       {children}
     </div>
   )
