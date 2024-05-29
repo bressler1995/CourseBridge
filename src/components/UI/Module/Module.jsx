5c5d333e165a64f849450ae46825fef2100d4a96
Brian Ressler <carlofabyss@hotmail.com>
2024-05-29 08:29:23 -0500

    Add reference to stack overflow article that could help with dynamic import

--- src/components/UI/Module/Module.jsx
+++ src/components/UI/Module/Module.jsx
@@ -5,7 +5,7 @@ function Module(file) {
 
   const params = useParams();
   
-
+  // TODO: Check this out- https://stackoverflow.com/questions/58785014/how-to-pass-string-into-react-router-dom-route-as-function
   return <h1>{params.id}</h1>;
   
 }
