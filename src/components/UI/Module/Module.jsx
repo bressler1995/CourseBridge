cb4501efeb3198ea8befe666ecd99dca4f4106e9
Brian Ressler <carlofabyss@hotmail.com>
2024-05-29 16:16:39 -0500

    Lets try using lazy loading to dynamically import
    It works on localhost but best to be sure it works with GitHub pages

--- src/components/UI/Module/Module.jsx
+++ src/components/UI/Module/Module.jsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react';
 import {useParams} from 'react-router-dom';
 import { createElement } from 'react';
 import parse from 'html-react-parser';
@@ -6,19 +7,27 @@ import Module1 from '../../../content/Module1.mdx';
 import Module2 from '../../../content/Module2.mdx';
 import Module3 from '../../../content/Module3.mdx';
 
+function renderLazyComponent(component) {
+  return lazy(() => import('../../../content/' + component));
+}
+
 function Module() {
 
   const params = useParams();
-  let currentModule = '<Module1/>';
+  let CurrentModule = <></>;
 
   toc.map((child, i) => {
     if(parseInt(child.id) == params.id) {
-      currentModule = '<' + child.file + '/>';
+      CurrentModule = renderLazyComponent(child.file + '.mdx');
     }
   });
   
   // TODO: Check this out- https://stackoverflow.com/questions/58785014/how-to-pass-string-into-react-router-dom-route-as-function
-  return <>{params.id == 1 ? <Module1/> : params.id == 2 ? <Module2/> : params.id == 3 ? <Module3/> : <Module1/>}</>;
+  return (
+    <Suspense fallback={<div>Loading</div>}>
+      <CurrentModule/>
+    </Suspense>
+  );
   
 }
 
