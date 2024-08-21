d4176db31deda3ef9f94d099d89d890320d145d4
Brian Ressler <carlofabyss@hotmail.com>
2024-08-21 13:59:25 -0500

    Add banner component to module readmes
    Titles look more inviting presented this way

--- src/components/UI/Module/Simple/SimpleSidebar.jsx
+++ src/components/UI/Module/Simple/SimpleSidebar.jsx
@@ -16,7 +16,12 @@ function SimpleSidebar({content}) {
 
   if(content != null && content != 0) {
       contentList = content.map((child, index) => {
-        return <li key={index}><Link to={'/Simple/' + idParam + '/' + lidParam + '/#' + child[1]}>{child[0]}</Link></li>
+        if(lidParam != null) {
+          return <li key={index}><Link to={'/Simple/' + idParam + '/' + lidParam + '/#' + child[1]}>{child[0]}</Link></li>
+        } else {
+          return <li key={index}><Link to={'/Simple/' + idParam + '/#' + child[1]}>{child[0]}</Link></li>
+        }
+        
       });
   }
 
@@ -44,38 +49,22 @@ function SimpleSidebar({content}) {
     return '';
   } else {
     
-    if(lidParam != null) {
-
-      return (<div className='os101SimpleSideBar'>
-        <div className='os101SimpleLogo'><img src='./images/logo.png'/></div>
-        <h2>All Content</h2>
-        <ul>
-            <li><Link to={'/Simple/' + idParam}>Welcome</Link></li>
-            {
-              lesson_data.map((child, index) => {
-                return <li key={index}><Link to={'/Simple/' + idParam + '/' + child[0]}>{child[1]}</Link></li>
-              })
-            }
-        </ul>
-        <h2>Navigate Lesson</h2>
-        <ul>
-        {contentList}
-        </ul>
-      </div>);
-    } else {
-      return (<div className='os101SimpleSideBar'>
-        <div className='os101SimpleLogo'><img src='./images/logo.png'/></div>
-        <h2>All Content</h2>
-        <ul>
-            <li><Link to={'/Simple/' + idParam}>Welcome</Link></li>
-            {
-              lesson_data.map((child, index) => {
-                return <li key={index}><Link to={'/Simple/' + idParam + '/' + child[0]}>{child[1]}</Link></li>
-              })
-            }
-        </ul>
-      </div>);
-    }
+    return (<div className='os101SimpleSideBar'>
+      <div className='os101SimpleLogo'><img src='./images/logo.png'/></div>
+      <h2>All Content</h2>
+      <ul>
+          <li><Link to={'/Simple/' + idParam}>Welcome</Link></li>
+          {
+            lesson_data.map((child, index) => {
+              return <li key={index}><Link to={'/Simple/' + idParam + '/' + child[0]}>{child[1]}</Link></li>
+            })
+          }
+      </ul>
+      <h2>Navigation</h2>
+      <ul>
+      {contentList}
+      </ul>
+    </div>);
     
   }
   
